e3233166fead83430ef609291c88855128484b69
Ehsan Khalid <s3838975@student.rmit.edu.au>
2022-04-29 09:47:54 +1000

    Implemented Node.h & Node.cpp, Created Board Class Skeleton Code

---
+++ Board.cpp
@@ -0,0 +1,18 @@
+#ifndef ASSIGN2_BOARD_H
+#define ASSIGN2_BOARD_H
+
+#include "Board.h"
+
+Board::Board(){
+
+}
+
+Board::Board(Board &other){
+
+}
+
+Board::~Board(){
+
+}
+
+#endif // ASSIGN2_BOARD_H
--- Board.h
+++ Board.h
@@ -1,5 +1,4 @@
-#ifndef ASSIGN2_BOARD_H
-#define ASSIGN2_BOARD_H
+
 #include <vector>
 
 class Board {
@@ -9,21 +8,8 @@ public:
     Board(Board &other);
     ~Board();
 
-    int size();
-
-    Board* get(int index);
-
-    void add(Board* board);
-
-    void add(Board* board, int index);
-
-    void remove(int index);
-
-    void clear();
-
 private:
 
     std::vector<Board*> tile;
 };
 
-#endif // ASSIGN2_BOARD_H
