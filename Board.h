dc1032722b5f337ff915cde1a85e3b653e1c1ec7
Ehsan Khalid <s3838975@student.rmit.edu.au>
2022-04-29 09:43:06 +1000

    Implemented Node.h & Node.cpp

--- Board.h
+++ Board.h
@@ -1,9 +1,29 @@
 #ifndef ASSIGN2_BOARD_H
 #define ASSIGN2_BOARD_H
+#include <vector>
 
 class Board {
-public:
 
+public:
+    Board();
+    Board(Board &other);
+    ~Board();
+
+    int size();
+
+    Board* get(int index);
+
+    void add(Board* board);
+
+    void add(Board* board, int index);
+
+    void remove(int index);
+
+    void clear();
+
+private:
+
+    std::vector<Board*> tile;
 };
 
 #endif // ASSIGN2_BOARD_H
--- Node.cpp
+++ Node.cpp
@@ -1,12 +1,21 @@
 
 #include "Node.h"
 
-Node::Node(Tile* tile, Node* next)
-{
-   // TODO
+Node::Node(Tile* tile, Node* next) {
+
+   this->tile = new Tile(*tile);
+   this->next = next;
+  
 }
 
-Node::Node(Node& other)
+Node::Node(Node& other) {
+
+   this->tile = new Tile(*other.tile);
+   this->next = other.next;
+
+}
+
+Node::~Node()
 {
-   // TODO
+    delete tile;
 }
